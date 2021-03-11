cce2e8c1e893f1f36b4ee497ffcfe7783e99891c
Alvaro Miranda Aguilera <kikitux@gmail.com>
2021-03-11 23:45:38 +0100

    update

--- scripts/provision_db.sh
+++ scripts/provision_db.sh
@@ -12,8 +12,9 @@ rpm -q oracle-database-preinstall-18c || {
 # check DB is installed
 rpm -q oracle-database-xe-18c || {
   [ -f /vagrant/sw/oracle-database-xe-18c-1.0-1.x86_64.rpm ] || {
-    echo ERR: missing sw/oracle-database-xe-18c-1.0-1.x86_64.rpm
-    exit 1
+    cd /vagrant/sw
+    wget https://download.oracle.com/otn-pub/otn_software/db-express/oracle-database-xe-18c-1.0-1.x86_64.rpm
   }
+  [ -d /opt/oracle/product/18c/dbhomeXE ] && rm -fr /opt/oracle/product/18c/dbhomeXE
   yum localinstall -y /vagrant/sw/oracle-database-xe-18c-1.0-1.x86_64.rpm
 }
--- scripts/provision_vault.sh
+++ scripts/provision_vault.sh
@@ -4,10 +4,14 @@ cp /vagrant/sw/hosts /etc/hosts
 echo 'vault.test' > /etc/hostname
 hostname vault.test
 
+export PATH=/usr/local/bin:${PATH}
+
 which vault || {
   yum install -y curl wget unzip jq
 
   curl -sL https://raw.githubusercontent.com/kikitux/curl-bash/master/vault-dev-inmem/vault.sh | bash
+
+  hash -r
 
   yes | cp  /vagrant/sw/vault.service /etc/systemd/system/vault.service
 
@@ -21,20 +25,22 @@ which vault || {
 
 }
 
-  export VAULT_ADDR=http://localhost:8200
+cp /root/.vault-token /home/vagrant/.vault-token
+chown -R vagrant: /home/vagrant/.vault-token
+export VAULT_ADDR=http://localhost:8200
 
-  vault write sys/plugins/catalog/database/oracle-database-plugin \
-    sha256="4625dd52e5c97e87d6b7366b12c419b155df71cfdc23bff44f05fbbf4caa9b3c" \
-    command=vault-plugin-database-oracle
+vault write sys/plugins/catalog/database/oracle-database-plugin \
+  sha256="4625dd52e5c97e87d6b7366b12c419b155df71cfdc23bff44f05fbbf4caa9b3c" \
+  command=vault-plugin-database-oracle
 
-  vault write database/config/my-oracle-database \
-    plugin_name=oracle-database-plugin \
-    connection_url="system/password@db.test:1521/XEPDB1" \
-    allowed_roles="my-role" \
+vault write database/config/my-oracle-database \
+  plugin_name=oracle-database-plugin \
+  connection_url="system/password@db.test:1521/XEPDB1" \
+  allowed_roles="my-role" \
 
-  vault write database/roles/my-role \
-    db_name=my-oracle-database \
-    creation_statements='CREATE USER {{username}} IDENTIFIED BY "{{password}}"; GRANT CONNECT TO {{username}}; GRANT CREATE SESSION TO {{username}};' \
-    default_ttl="1h" \
-    max_ttl="24h"
+vault write database/roles/my-role \
+  db_name=my-oracle-database \
+  creation_statements='CREATE USER {{username}} IDENTIFIED BY "{{password}}"; GRANT CONNECT TO {{username}}; GRANT CREATE SESSION TO {{username}};' \
+  default_ttl="1h" \
+  max_ttl="24h"
 
