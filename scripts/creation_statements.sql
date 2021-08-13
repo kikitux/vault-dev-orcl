ad8542965d3e168ac8264acbca33337cc4a63e6e
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2021-08-13 10:21:13 +0200

    Update creation_statements.sql

--- scripts/creation_statements.sql
+++ scripts/creation_statements.sql
@@ -1,5 +1,6 @@
 CREATE USER {{username}} IDENTIFIED BY "{{password}}";
 GRANT CONNECT TO {{name}};
+GRANT CREATE SESSION to {{name}};
 GRANT DBA TO {{name}};
 GRANT RESOURCE TO {{name}};
 CREATE OR REPLACE TRIGGER vault_schema_logon
